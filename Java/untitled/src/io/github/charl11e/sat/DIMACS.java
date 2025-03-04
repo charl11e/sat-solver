e8bce818f220a70ce72cb87c3f306200fe4fd1e6
charl11e <146877892+charl11e@users.noreply.github.com>
2025-03-04 02:48:31 +0000

    Completed DIMACS loader

--- Java/untitled/src/io/github/charl11e/sat/DIMACS.java
+++ Java/untitled/src/io/github/charl11e/sat/DIMACS.java
@@ -5,22 +5,55 @@ import java.io.IOException;
 
 public class DIMACS {
 
-    public static void load (String file) {
+    public static ArrayList<ArrayList<Integer>> load (String file) {
+        // Declare ArrayList to store list of lists of clauses
+        ArrayList<ArrayList<Integer>> clauses = new ArrayList<>();
 
-        ArrayList<ArrayList> clauses = new ArrayList<>();
+        // Load file path and get contents of file into String
         Path filePath = Paths.get("./files", file);
-
+        String content = "";
         try {
-            String content = Files.readString(filePath);
-            System.out.println(content);
+            content = Files.readString(filePath);
         } catch (IOException e) {
             e.printStackTrace();
+            return clauses;
         }
-    };
+
+        // Split String by lines
+        String[] lines = content.split("\n");
+
+        // Parse individual clauses from file into clauses
+        for (String line : lines) {
+            // If first character is p or c, skip
+            if (line.charAt(0) == 'p' || line.charAt(0) == 'c') {
+              continue;
+            }
+
+            // Otherwise, split up line by spaces
+            String parts[] = line.split(" ");
+            ArrayList<Integer> clause = new ArrayList<>();
+
+            // Parse into integers
+            for (String part : parts) {
+                int literal = Integer.parseInt(part);
+                if (literal == 0 ) {
+                    break;
+                }
+                clause.add(literal);
+            }
+
+            // Add clause into list of clauses
+            clauses.add(clause);
+
+        }
+
+        return clauses;
+
+    }
 
 
     public static void main(String[] args) {
-        DIMACS.load("testsat.txt");
+        System.out.println(DIMACS.load("testsat.txt"));
     }
 
 }
