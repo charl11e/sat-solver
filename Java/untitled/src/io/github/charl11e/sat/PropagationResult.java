70feb568d772354de98bbfa3b08dffae1b2dd944
charl11e <146877892+charl11e@users.noreply.github.com>
2025-03-06 00:19:58 +0000

    Revert "Started work on Pure Literal Elimination, added .gitignore"
    This reverts commit 14ce770698ee9eea14e2f88f444c631514c04eca.

--- Java/untitled/src/io/github/charl11e/sat/PropagationResult.java
+++ Java/untitled/src/io/github/charl11e/sat/PropagationResult.java
@@ -1,11 +1,11 @@
 package io.github.charl11e.sat;
 import java.util.ArrayList;
 
-public class SATResult {
+public class PropagationResult {
     private final ArrayList<ArrayList<Integer>> clause_set;
     private final ArrayList<Integer> assignment;
 
-    public SATResult(ArrayList<ArrayList<Integer>> clause_set, ArrayList<Integer> assignment) {
+    public PropagationResult(ArrayList<ArrayList<Integer>> clause_set, ArrayList<Integer> assignment) {
         this.clause_set = clause_set;
         this.assignment = assignment;
     }
